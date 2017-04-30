b7b58c7db31df7374f593facf4313b8eb62a58c8
bhaddad5 <bhaddad5@gmail.com>
2017-04-29 20:52:24 -0700

    DamageRecievers implemented

---
+++ Assets/Enemies/DamageReciever.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageReciever : MonoBehaviour
+{
+	public float health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		var bullet = other.GetComponent<Bullet>();
+		if (bullet != null)
+		{
+			health -= bullet.damage;
+			Destroy(bullet.gameObject);
+			if(health <=0)
+				Destroy(gameObject);
+		}
+	}
+}
