e3c2bd5543fb561bbbfc5200e28851977a676a6f
bhaddad5 <bhaddad5@gmail.com>
2017-05-01 22:30:27 -0700

    Added tmp enemy spawn points to scene

---
+++ Assets/TMP_EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TMP_EnemySpawner : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+	public int NumToMaintain;
+
+	private List<GameObject> enemies = new List<GameObject>();
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = enemies.Count-1; i >=0 ; i--)
+		{
+			if (enemies[i] == null)
+				enemies.RemoveAt(i);
+		}
+
+		if (enemies.Count < NumToMaintain)
+			enemies.Add(Instantiate(enemyPrefab, transform.position, Quaternion.identity));
+	}
+}
