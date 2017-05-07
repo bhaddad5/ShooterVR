ef7af19f6835d618c7714fe46c46982ddbcfa66f
bhaddad5 <bhaddad5@gmail.com>
2017-05-06 22:33:25 -0700

    added laser sight to sniper

---
+++ Assets/Weapons/LaserPointerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointerController : MonoBehaviour
+{
+	Vector3 startScale;
+	float globalStartScale;
+	void Start()
+	{
+		startScale = transform.localScale;
+		globalStartScale = transform.lossyScale.z;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(new Ray(transform.position, transform.forward), out hit, float.MaxValue))
+		{
+			//TODO: Why x10?
+			transform.localScale = new Vector3(startScale.x, startScale.y, (hit.distance)*10);
+		}
+		else transform.localScale = new Vector3(startScale.x, startScale.y, 1000f);
+	}
+}
